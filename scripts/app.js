cdf752c86ad5f1771d889d74f3a5402f02460bdf
qiwei li <sirily1997@gmail.com>
2018-08-11 03:39:37 -0500

    fixed cannot type lyrics problem

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,10 @@
 import {
 	MDCTextField
 } from '@material/textfield';
-import mdcAutoInit from '@material/auto-init';
-try {
-    window.onload(function (e) {
-        const username_field = new MDCTextField(document.querySelector('.mdc-text-field'));
-    });
-}catch (e) {
 
-}
+const username_field = new MDCTextField(document.querySelector('.mdc-text-field--textarea'));
+
+
 
 
 
--- scripts/bundle.js
+++ scripts/bundle.js
@@ -2038,17 +2038,7 @@ exports.strings = strings;
 
 var _textfield = __webpack_require__(15);
 
-var _autoInit = __webpack_require__(32);
-
-var _autoInit2 = _interopRequireDefault(_autoInit);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-try {
-    window.onload(function (e) {
-        var username_field = new _textfield.MDCTextField(document.querySelector('.mdc-text-field'));
-    });
-} catch (e) {}
+var username_field = new _textfield.MDCTextField(document.querySelector('.mdc-text-field--textarea'));
 
 /***/ }),
 /* 15 */
@@ -5753,114 +5743,6 @@ var MDCNotchedOutlineFoundation = function (_MDCFoundation) {
 }(_foundation2.default);
 
 exports.default = MDCNotchedOutlineFoundation;
-
-/***/ }),
-/* 32 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = mdcAutoInit;
-/**
- * Copyright 2016 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-var registry = Object.create(null);
-
-var CONSOLE_WARN = console.warn.bind(console);
-
-function _emit(evtType, evtData) {
-  var shouldBubble = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
-
-  var evt = void 0;
-  if (typeof CustomEvent === 'function') {
-    evt = new CustomEvent(evtType, {
-      detail: evtData,
-      bubbles: shouldBubble
-    });
-  } else {
-    evt = document.createEvent('CustomEvent');
-    evt.initCustomEvent(evtType, shouldBubble, false, evtData);
-  }
-
-  document.dispatchEvent(evt);
-}
-
-/**
- * Auto-initializes all mdc components on a page.
- */
-function mdcAutoInit() {
-  var root = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : document;
-  var warn = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : CONSOLE_WARN;
-
-  var components = [];
-  var nodes = root.querySelectorAll('[data-mdc-auto-init]');
-  for (var i = 0, node; node = nodes[i]; i++) {
-    var ctorName = node.dataset.mdcAutoInit;
-    if (!ctorName) {
-      throw new Error('(mdc-auto-init) Constructor name must be given.');
-    }
-
-    var Ctor = registry[ctorName];
-    if (typeof Ctor !== 'function') {
-      throw new Error('(mdc-auto-init) Could not find constructor in registry for ' + ctorName);
-    }
-
-    if (node[ctorName]) {
-      warn('(mdc-auto-init) Component already initialized for ' + node + '. Skipping...');
-      continue;
-    }
-
-    // TODO: Should we make an eslint rule for an attachTo() static method?
-    var component = Ctor.attachTo(node);
-    Object.defineProperty(node, ctorName, {
-      value: component,
-      writable: false,
-      enumerable: false,
-      configurable: true
-    });
-    components.push(component);
-  }
-
-  _emit('MDCAutoInit:End', {});
-  return components;
-}
-
-mdcAutoInit.register = function (componentName, Ctor) {
-  var warn = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : CONSOLE_WARN;
-
-  if (typeof Ctor !== 'function') {
-    throw new Error('(mdc-auto-init) Invalid Ctor value ' + Ctor + '. Expected function');
-  }
-  if (registry[componentName]) {
-    warn('(mdc-auto-init) Overriding registration for ' + componentName + ' with ' + Ctor + '. ' + ('Was: ' + registry[componentName]));
-  }
-  registry[componentName] = Ctor;
-};
-
-mdcAutoInit.deregister = function (componentName) {
-  delete registry[componentName];
-};
-
-mdcAutoInit.deregisterAll = function () {
-  Object.keys(registry).forEach(this.deregister, this);
-};
 
 /***/ })
 /******/ ]);
